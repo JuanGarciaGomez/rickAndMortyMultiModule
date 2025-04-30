ba08c9223d6e5bad87a267ef8a25f8ed3bd46d1e
Juan Felipe Garcia <70071051+JuanGarciaGomez@users.noreply.github.com>
2025-04-30 09:53:42 -0500

    init project

---
+++ app/src/test/java/com/project/jf/rickAndMortyMultiModule/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.project.jf.rickAndMortyMultiModule
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * rickAndMortyMultiModule local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class rickAndMortyMultiModuleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
